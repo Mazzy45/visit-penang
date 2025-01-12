3413734decc4c916b739eb7dbab5349a5c804d8c
Mazzy45 <nurmaziah45@gmail.com>
2025-01-12 14:24:30 +0800

    Updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
 import './App.css'; // Import your CSS styles
 
 // Import components
